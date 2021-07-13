4a31d78c4165caa80e960a3067d6e80aafe25d3d
unmo0 <unmo001@gmaill.com>
2021-07-13 16:29:37 +0900

    モデル変更

--- registration/models.py
+++ registration/models.py
@@ -5,6 +5,16 @@ from django.db import models
 
 # Create your models here.
 
+class Prefecture(models.Model):
+    prefecture = models.CharField(verbose_name='都道府県', max_length=3)
+
+    class Meta:
+        verbose_name_plural = '都道府県'
+
+    def __str__(self):
+        return self.prefecture
+
+
 class MyUserManager(BaseUserManager):
     def create_user(self, email, date_of_birth, password=None):
         if not email:
@@ -28,8 +38,8 @@ class CustomUser(AbstractBaseUser):
         (male, '男'),
         (female, '女')
     ]
+    prefecture = models.ForeignKey(Prefecture, on_delete=models.CASCADE)
     gender = models.CharField(verbose_name='性別', max_length=2, choices=gender_choice, null=False, blank=False)
-    prefecture = models.CharField(verbose_name='都道府県', max_length=10, null=False, blank=False)
     purpose = models.CharField(verbose_name='ご登録目的', max_length=2, null=False, blank=False)
     scene = models.CharField(verbose_name='ご利用シーン', max_length=6, null=False, blank=False)
 
@@ -39,6 +49,9 @@ class Service(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=50)
     description = models.CharField(verbose_name='説明', max_length=500)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural='サービス'
 
     def __str__(self):
         return self.title
@@ -68,10 +81,11 @@ class Seller(models.Model):
 
 # メッセージ
 class Message(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    text = models.CharField(verbose_name='テキスト', max_length=250)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     Buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    text = models.CharField(verbose_name='テキスト', max_length=250)
     published_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -82,13 +96,12 @@ class Message(models.Model):
 
 
 class MessageRoom(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    service = models.ForeignKey(Service,on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     Seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     Buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
-
+    purchase_review = models.IntegerField(verbose_name='購入実績', default=0, null=True, blank=True)
+    exhibit_review = models.IntegerField(verbose_name='販売実績', default=0, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'チャットルーム'
-
-
