29f73ba05f20339e1e2cdf57780f26a4617b34f9
unmo0 <unmo001@gmaill.com>
2021-07-14 17:55:13 +0900

    ご確認お願いします。

--- registration/admin.py
+++ registration/admin.py
@@ -1,2 +1,20 @@
-from django.contrib import admin
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class AdminUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'is_staff')
+    search_fields = ('username', 'full_name', 'email')
+    filter_horizontal = ('groups', 'user_permissions')
