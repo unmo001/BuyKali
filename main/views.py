b5930312f9653f84bc95796bb94fbe6711778ca8
unmo0 <unmo001@gmaill.com>
2021-07-14 16:39:42 +0900

    エラー発生ご確認お願いします。

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.urls import reverse_lazy
 from django.views.generic import ListView, FormView
 
 from main.forms import MessageForm
@@ -9,6 +10,7 @@ class MessageRoomView(FormView):
     template_name = 'main/message_room.html'
     model = Message
     form_class = MessageForm
+    success_url = reverse_lazy('main:message_room')
 
     def get_context_data(self, **kwargs):
         context = super(MessageRoomView, self).get_context_data(**kwargs)
--- registration/models.py
+++ registration/models.py
@@ -1,60 +1,21 @@
-import uuid as uuid_lib
-
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
-from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.core.mail import send_mail
+from django.contrib.auth.models import UserManager, AbstractUser
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 class Prefecture(models.Model):
-    prefecture = models.CharField(verbose_name='都道府県', max_length=3)
+    prefecture_choice = models.CharField(verbose_name='都道府県', max_length=3)
 
     class Meta:
         verbose_name_plural = '都道府県'
 
     def __str__(self):
-        return self.prefecture
+        return self.prefecture_choice
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    uuid = models.UUIDField(default=uuid_lib.uuid4, primary_key=True, editable=False)
-    username_validator = UnicodeUsernameValidator()
-
-    username = models.CharField(
-        _('username'),
-        max_length=150,
-        unique=True,
-        help_text=_(
-            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'
-        ),
-        validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        }
-    )
-    full_name = models.CharField(_('氏名'),max_length=150,blank=True)
-    email = models.EmailField(_('email address'),blank=True)
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_(
-            'Designates whether the user can log into this admin site.'),
-        )
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active.'
-            'Unselect this instead of deleting accounts accounts.'
-        ),
-    )
-    date_joined = models.DateTimeField(_('date joined'),default=timezone.now)
-
+class CustomUser(AbstractUser):
     male = '男'
     female = '女'
     gender_choice = [
@@ -70,24 +31,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
-
+    REQUIRED_FIELDS = ['email', ]
 
     class Meta:
         verbose_name_plural = 'ユーザー'
-
-    def clean(self):
-        super(CustomUser, self).clean()
-        self.email = self.__class__.objects.normalize_email(self.email)
-
-    def email_user(self,subject,message,form_email=None,**kwargs):
-        send_mail(subject,message,form_email,[self.email],**kwargs)
-
-    def get_full_name(self):
-        return self.full_name
-
-    def get_short_name(self):
-        return self.full_name
 
 
 # サービス
